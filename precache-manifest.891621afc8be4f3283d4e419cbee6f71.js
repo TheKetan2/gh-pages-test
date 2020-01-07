8b0f64e720e683a2b4d3b82f955601bb230af645
Ketan <ketantoo@gmail.com>
2020-01-07 20:19:15 +0530

    Updates

---
+++ precache-manifest.891621afc8be4f3283d4e419cbee6f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f7785f2202f8f73318db76be3bc613",
+    "url": "/youtubeapp/index.html"
+  },
+  {
+    "revision": "51eef512325b4dbde4d1",
+    "url": "/youtubeapp/static/css/main.9f64764d.chunk.css"
+  },
+  {
+    "revision": "f7780f2d4f3bf26f6b7d",
+    "url": "/youtubeapp/static/js/2.24e023da.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/youtubeapp/static/js/2.24e023da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51eef512325b4dbde4d1",
+    "url": "/youtubeapp/static/js/main.8a39d6ed.chunk.js"
+  },
+  {
+    "revision": "df5e3fc041145aabd88a",
+    "url": "/youtubeapp/static/js/runtime-main.ab55fa8e.js"
+  }
+]);
